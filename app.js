3d42311bd0e8a084ea58a211b4c17b60fce5b12c
Kulbhushan goswami <igoswamik457@gmail.com>
2021-02-11 17:14:51 +0530

    PricingPanel commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// (function(){
+//     let input=prompt("Enter your choice for todo list(new/delete/list/quit)");
+//     let arr=[];
+//     while(input!="quit"){
+//         if(input==="new"){
+//             let ele=prompt("what do you want to add to your todo list?");
+//             arr.push(ele);
+//         }
+//         else if(input==="delete"){
+//             let i=prompt("enter index of element you want to delete");
+//             arr.splice(i,1);
+
+//         }
+//         else{
+//             for(let i=0;i<arr.length;i++){
+//                 console.log(`${i} : ${arr[i]}`);
+//             }
+//         }
+//        input=prompt("Enter next your choice");
+//     }
+//     console.log("you quit!!");
+// })();
+
+    
